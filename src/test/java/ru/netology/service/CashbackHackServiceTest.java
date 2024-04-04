3fa9486d4b334161271ae0d510fedd26cbd4c283
EvgeniyaNesmeyanova <prof_nea@mail.ru>
2024-04-04 16:57:54 +0500

    explore the work of TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.service;
+
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculate() { //проверка работы сервиса на положительную величину меньше 1000 руб
+        CashbackHackService service = new CashbackHackService();
+        int amount=900;
+
+        int actual=service.remain(amount);
+        int expected=100;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldCalculateMin() { //проверка работы сервиса на нулевую сумму
+        CashbackHackService service = new CashbackHackService();
+        int amount=0;
+
+        int actual=service.remain(amount);
+        int expected=1000;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldCalculateMax() { //покупка ровно на 1000 руб
+        CashbackHackService service = new CashbackHackService();
+        int amount=1_000;
+
+        int actual=service.remain(amount);
+        int expected=0;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldCalculateMaxMax() { //покупка ровно на 1001 руб
+        CashbackHackService service = new CashbackHackService();
+        int amount=1_001;
+
+        int actual=service.remain(amount);
+        int expected=999;
+
+        assertEquals(actual, expected);
+    }
+}
